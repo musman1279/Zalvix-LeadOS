c7d2ed0cceb1bf2d955fe93cda3e102de799a4ab
musman1279 <musman1279@gmail.com>
2026-08-04 00:39:08 +0500

    working send token.js

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import ApiError from "../utils/ApiError.js";
 import catchAsync from "../utils/catchAsync.js";
+import sendToken from "../utils/sendToken.js";
 
 export const registerUser = catchAsync(async (req, res, next) => {
   // 1. Get Data From Request Body
@@ -31,3 +32,35 @@ export const registerUser = catchAsync(async (req, res, next) => {
     message: "Account created successfully. Please login to continue.",
   });
 });
+
+
+// create Login User Controller
+
+export const loginUser = catchAsync(async (req, res, next) => {
+    // 1. Get Email & Password
+    const { email, password } = req.body;
+
+    // 2. Validate Input
+    if (!email || !password) {
+        return next(new ApiError("Please enter email and password", 400));
+    }
+
+    // 3. Find User (Include Password)
+    const user = await User.findOne({ email }).select("+password");
+
+    // 4. Check User Exists
+    if (!user) {
+        return next(new ApiError("Invalid email or password", 401));
+    }
+
+    // 5. Compare Password
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if (!isPasswordMatched) {
+        return next(new ApiError("Invalid email or password", 401));
+    }
+
+    // 6. Send JWT Token
+    sendToken(user, 200, res, "Login successful");
+});
+
---
+++ server/src/utils/sendToken.js
@@ -0,0 +1,32 @@
+const sendToken = (user, statusCode, res, message) => {
+    // Generate JWT Token
+    const token = user.getJwtToken();
+
+    // Cookie Options
+    const options = {
+        expires: new Date(
+            Date.now() +
+                Number(process.env.COOKIE_EXPIRE) * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+    };
+
+    // Send Cookie & Response
+    res
+        .status(statusCode)
+        .cookie("token", token, options)
+        .json({
+            success: true,
+            message,
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+            },
+        });
+};
+
+export default sendToken;
